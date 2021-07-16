2129e282ab1cb6a54a36974547c3f8a39caf79a0
Yan-Luca <36674649+chaosfreak93@users.noreply.github.com>
2021-07-16 22:31:43 +0200

    alt:V WebSocketClient (#5)
    * Switched to coreclr .net5.0-rework
    * Use new alt:V WebSocketClient
    * Updated TypeScript
    * Compiled TypeScript
    Thanks to @chaosfreak93

--- SaltyChat.Server/VoiceManager.cs
+++ SaltyChat.Server/VoiceManager.cs
@@ -18,7 +18,7 @@ namespace SaltyChat.Server
     {
         #region Useless things.. But I like them
 
-        private const string Version = "1.0.9"; // ToDo: Change on update
+        private const string Version = "1.0.10"; // ToDo: Change on update
 
         #endregion
 
