89118d54ddb7cdd654c12c5fa86d59715f406dbc
Lorian <lorianatashi@icloud.com>
2021-12-11 23:02:31 +0100

    added new Radiotype Enums

--- SaltyChat.Client/source/Enum/SaltyChat/RadioType.ts
+++ SaltyChat.Client/source/Enum/SaltyChat/RadioType.ts
@@ -1,7 +1,10 @@
 export enum RadioType {
-    none = 1,
-    shortRange = 2,
-    longRange = 4,
-    distributed = 8,
-    ultraShortRange = 16
+  none = 1,
+  shortRange = 2,
+  longRange = 4,
+  distributed = 8,
+  ultraShortRange = 16,
+  addRadioChannelMember = 37,
+  updateRadioChannelmembers = 38,
+  removeRadioChannelMember = 39,
 }
