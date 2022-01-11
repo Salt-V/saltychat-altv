91d5b8f84a6a06cd6ef4081c9599ec93dae0682e
Phill <philltv.gaming@gmail.com>
2022-01-11 19:36:41 +0100

    Added States

---
+++ SaltyChat.Server/Shared/State.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaltyChat.Server.Shared
+{
+    public static class State
+    {
+        #region Player States
+        public const string SaltyChat_TeamSpeakName = "SaltyChat_TeamSpeakName";
+        public const string SaltyChat_VoiceRange = "SaltyChat_VoiceRange";
+        public const string SaltyChat_IsAlive = "SaltyChat_IsAlive";
+        public const string SaltyChat_IsUsingMegaphone = "SaltyChat_IsUsingMegaphone";
+        #endregion
+
+        #region Global States
+        public const string SaltyChat_RadioChannelMember = "SaltyChat_RadioChannelMember";
+        public const string SaltyChat_RadioChannelSender = "SaltyChat_RadioChannelSender";
+        #endregion
+    }
+}
