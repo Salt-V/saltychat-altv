a8c802b291fef46e4985a6d1dff882a87cd2c17c
Phill <philltv.gaming@gmail.com>
2022-01-11 18:30:02 +0100

    Added voiceChangeHandlerCookies

--- SaltyChat.Client/source/Models/SaltyChat/RadioConfiguration.ts
+++ SaltyChat.Client/source/Models/SaltyChat/RadioConfiguration.ts
@@ -1,6 +1,8 @@
 export class RadioConfiguration {
     public primaryChannel: string;
+    public primaryChangeHandlerCookies: number[];
     public secondaryChannel: string;
+    public SecondaryChangeHandlerCookies: number[];
     public volume: number = 1;
     public usingPrimaryRadio: boolean;
     public usingSecondaryRadio: boolean;
